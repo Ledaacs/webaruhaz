c468b2222d5649262ce772cc2d6c15073d8ba0bb
ledacsbenya <ledacsbence@gmail.com>
2023-01-27 12:08:34 +0100

    javítás

--- public/my.js
+++ public/my.js
@@ -1,1 +1,35 @@
 console.log("frontend: helo");
+
+
+
+
+async function getProducts(){
+    let response = await fetch(url);
+    let data = await response.json
+
+    console.log(data);
+    
+}
+
+async function postProduct(){
+    let body = {
+        name: "Á9",
+        quantity: 99,
+        price: 6500,
+        type: "fémáru"
+    }
+
+
+
+    let response = await fetch(url, {
+        Method: 'POST',
+        Headers: {
+          Accept: 'application.json',
+          'Content-Type': 'application/json'
+        },
+        Body: body,
+        Cache: 'default'
+      })
+}
+
+getProducts();
